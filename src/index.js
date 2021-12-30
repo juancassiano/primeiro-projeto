2aebde4bce952bf572bdbb463f3c3e784ea2adf3
juan <juancassiano@hotmail.com.br>
2021-12-30 09:40:17 -0300

    create deposit

--- src/index.js
+++ src/index.js
@@ -50,6 +50,22 @@ app.post("/account", (request, response) =>{
     return response.status(201).send();
 })
 
+app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = { 
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
 app.get("/statement", verifyIfExistAccountCPF, (request,response) => {
 
     const { customer } = request;
